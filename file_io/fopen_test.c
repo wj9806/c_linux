6570e3b889acff716e9318176243d695cfb2bbbf
wenjie <wenjie204811@gmail.com>
2024-08-21 11:10:01 +0800

    first commit

---
+++ file_io/fopen_test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /**
+     * const char *__restrict __filename, 文件名
+	 * const char *__restrict __modes   访问模式 
+     *  r 只读
+     *  w 只写
+     *  a 只追加写模式 
+     *  r+: 读写模式  写入是从头一个一个覆盖
+     *  w+: 读写模式  如果文件存在清空文件
+     *  a+: 读写追加模式 如果文件存在末尾追加写
+     * 
+     * return: FILE* 
+     */
+    FILE * f = fopen("io.txt", "w");
+    if (f == NULL) {
+        printf("open file failed!\n");
+    } else {
+        printf("open file success!\n");
+    }
+    
+    return 0;
+}
