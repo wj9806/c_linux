a55e23e8e3acf79af3e4e0bbeb92a1265eab6821
wenjie <wenjie204811@gmail.com>
2024-08-21 11:36:30 +0800

    fputc fputs函数

---
+++ file_io/fputs_test.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /**
+     * const char *__restrict __filename, 文件名
+	 * const char *__restrict __modes   访问模式 
+     *  r 只读
+     *  w 只写
+     *  a 只追加写模式 
+     *  r+: 读写模式  写入是从头一个一个覆盖
+     *  w+: 读写模式  如果文件存在清空文件
+     *  a+: 读写追加模式 如果文件存在末尾追加写
+     * 
+     * return: FILE* 
+     */
+    FILE * f = fopen("io.txt", "w");
+    if (f == NULL) {
+        printf("open file failed\n");
+    } else {
+        printf("open file success\n");
+    }
+
+    /**
+     * return 成功返回非负数 失败返回EOF
+     */
+    int putc_res = fputs("hello world", f);
+    if (putc_res == EOF) {
+        printf("puts file failed\n");
+    } else {
+        printf("puts file success\n");
+    }
+    
+    
+    /**
+     * return 成功返回0 失败返回EOF(负数)
+     */
+    int res = fclose(f);
+    if (res == EOF) {
+        printf("close file failed\n");
+    } else {
+        printf("close file success\n");
+    }
+    
+    return 0;
+}
